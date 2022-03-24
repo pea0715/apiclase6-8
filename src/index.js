72cec61161d270e4f8bb2a317fc5a7c5cd5b067a
pea0715 <pabloespinosarias11c@gmail.com>
2022-03-23 19:41:03 -0500

    controladores de usuario

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log("Pendejo")
+// importacion del fremework 
+const express = require('express')
+
+// creando la aplicacion 
+const app = express()
+
+// integrando el router con la aplicacion
+const router =  require("./routers/index.routers")
+app.use(router)
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+/// puerto donde se levanta el servidor web 
+// puerto donde esta escuchando la API 
+const port = 3000
+
+//levantar la API que estara escuchando en el puerto  3000
+// 1.Primer parametro: puerto 
+// 2.Segundo parametro: callback
+app.listen(port, () => {
+  console.log(`API escuchando en: http://localhost:${port}`)
+})
